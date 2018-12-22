19a567c7b2350a8ed92dd636ff8fcb619ec5952d
zengyy <123395623@qq.com>
2018-12-23 00:46:51 +0800

    如果没有配置词库的目录则取当前dll所在的目录下的Dict_Jieba为词库目录

--- src/Analyser/ConfigManager.cs
+++ src/Analyser/ConfigManager.cs
@@ -12,7 +12,16 @@ namespace JiebaNet.Analyser
             get
             {
 #if !(NETSTANDARD1_0 || NETSTANDARD2_0)
-                var configFileDir = ConfigurationManager.AppSettings["JiebaConfigFileDir"] ?? "Resources";
+                var configFileDir = ConfigurationManager.AppSettings["JiebaConfigFileDir"];
+                if (configFileDir == null)
+                {
+                    // 如果没有配置词库的目录则取当前dll所在的目录下的Dict_Jieba为词库目录
+                    var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                    var uri = new UriBuilder(codeBase);
+                    var path = Uri.UnescapeDataString(uri.Path);
+                    configFileDir = Path.Combine(Path.GetDirectoryName(path), "Dict_Jieba");
+                }
+
                 if (!Path.IsPathRooted(configFileDir))
                 {
                     var domainDir = AppDomain.CurrentDomain.BaseDirectory;
